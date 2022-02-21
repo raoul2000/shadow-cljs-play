7b88e47cf8920fa8148859970df50952a33378b7
raoul2000 <manu34@free.fr>
2022-02-22 00:29:45 +0100

    trying to use the auto-complete component ..but failed

--- src/main/calc/application.cljs
+++ src/main/calc/application.cljs
@@ -1,8 +1,44 @@
 (ns calc.application
   (:require [reagent.dom :as rdom]
+            [reagent.core :as r]
             ["primereact/button" :as but]
             ["primereact/splitbutton" :as spbut]
-            ["primereact/dropdown" :as dropdown]))
+            ["primereact/dropdown" :as dropdown]
+            ["primereact/autocomplete" :as autoc]))
+
+(def state (r/atom {:all-countries      ["paris" "londres" "madrid" "marseille"]
+                    :filtered-countries []
+                    :selected-country   ""}))
+
+(defn on-change [ev]
+  (let [value (.-value ev)]
+    (js/console.log "onChange : " value)
+    (swap! state (fn [o]
+                   (update o :selected-country (fn [prev] 
+                                                 (str prev value)))))
+    ))
+
+(defn complete-method [ev]
+  (let [q (.-query ev)]
+    (js/console.log "complete: " q (count q))
+    (swap! state (fn [o]
+                   (prn (:all-countries o))
+                   (assoc o :filtered-countries
+                          (if (= 0 (count q))
+                            (:all-countries o)
+                            (filter #(.startsWith % q) (:all-countries o)))
+                          )))))
+
+
+(defn my-auto-complete []
+  (let [{:keys [filtered-countries selected-country]} @state]
+    
+        [:> autoc/AutoComplete {:value           selected-country
+                                :suggestions     filtered-countries
+                                :dropdown        true
+                                :completeMethod  complete-method
+                                :onChange        on-change}]
+    ))
 
 (defn render []
   (rdom/render
@@ -20,7 +56,7 @@
                      :iconPos "left"
                      :icon    "pi pi-check"}]
 
-         ;; https://www.primefaces.org/primereact/splitbutton/
+     ;; https://www.primefaces.org/primereact/splitbutton/
      [:> spbut/SplitButton {:label "Splitted"
                             :className "mr-2 mb-2"
                             :model [{:label "upload"
@@ -39,6 +75,8 @@
                            :options [{:name "paris" :code "p"}
                                      {:name "london" :code "lf"}]}]
 
+    [:h2 "Auto Complete"]
+    (my-auto-complete)
 
     ;;
     ]
